08c9fefa932ba2dc02899d4d765f799a9bf6e040
krishna143010 <krishna143010@gmail.com>
2023-11-09 23:38:04 -0500

    added core-jms app to handle messages and email service

---
+++ core-jms/src/main/java/com/krushna/corejms/CoreJmsApplication.java
@@ -0,0 +1,13 @@
+package com.krushna.corejms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreJmsApplication.class, args);
+	}
+
+}
---
+++ core-jms/src/test/java/com/krushna/corejms/CoreJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krushna.corejms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
